0ba1f434130422d250a141551454d820508fd84b
Arthur Stolyar <nekr.fabula@gmail.com>
2015-07-15 12:38:25 +0300

    Fixes & Updates

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var jsx = require('jsx-runtime');
 var escape = require('escape-html');
-var Tag = require('./tag');
+var Tag = require('./lib/tag');
+var hasOwn = Object.prototype.hasOwnProperty;
 
 var renderer = jsx.register('HTML', {
   tags: {
     '*': {
       enter: function(tag, props) {
-        return new Tag(tag, props);
+        return new Tag(escape(tag), props);
       },
       leave: function(parent, tag) {
         return parent;
@@ -14,29 +15,17 @@ var renderer = jsx.register('HTML', {
       child: function(child, parent) {
         if (child == null) return parent;
 
-        if (typeof child === 'string') {
-          child = escape(child);
+        if (child instanceof Tag) {
+          parent.children.push(child);
+        } else {
+          child = escape(child + '');
         }
 
-        parent.children.push(child);
         return parent;
       },
       props: function(props) {
-        return Object.keys(props).map(function(key) {
-          var val = key && props[key];
-
-          if (!key || val == null) return '';
-          if (val instanceof Tag) return '';
-
-          if (typeof val === 'string') {
-            val = JSON.stringify(val);
-            val = val.slice(1, val.length - 1);
-          } else {
-            val = JSON.stringify(val);
-          }
-
-          return key + '="' + escape(val) + '"';
-        }).join(' ');
+        return Object.keys(props)
+          .map(mapProps).join(' ');
       }
     }
   },
@@ -46,3 +35,48 @@ var renderer = jsx.register('HTML', {
 });
 
 module.exports = renderer;
+
+function mapProps(key) {
+  var val = key && props[key];
+
+  if (!key || val == null) return '';
+  if (val instanceof Tag) return '';
+
+  if (key === 'className') key = 'class';
+  else if (key === 'cssFor') key = 'for';
+  else key = key.toLowerCase();
+
+  if (key === 'style') {
+    val = handleStyle(val);
+  }
+
+  if (typeof val === 'string') {
+    // do nothing
+  } else {
+    val = JSON.stringify(val);
+  }
+
+  return escape(key) + '="' + escape(val) + '"';
+}
+
+function handleStyle(style) {
+  if (typeof style === 'string') return style;
+
+  var string = '';
+
+  for (var key in style) {
+    if (!hasOwn.call(style, key)) continue;
+
+    key = key.replace(/[A-Z]/g, function(m) {
+      return '-' + m.toLowerCase();
+    });
+
+    if (key.search(/moz-|webkit-|o-|ms-/) === 0) {
+      key = '-' + key;
+    }
+
+    string += key + ': ' + val + ';';
+  }
+
+  return string;
+}
